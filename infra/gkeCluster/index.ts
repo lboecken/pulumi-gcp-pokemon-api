1ce1b975c5238c2a1f1958b2ab38d24ad66b6d23
Lennart Boecken <lsboecken@gmail.com>
2022-07-12 10:48:03 -0400

    in progress

--- infra/gkeCluster/index.ts
+++ infra/gkeCluster/index.ts
@@ -1,1 +1,8 @@
-import * as k8 from '@';
+import * as k8s from '@pulumi/kubernetes';
+import * as gcp from '@pulumi/gcp';
+
+const engineVersion = gcp.container
+  .getEngineVersions()
+  .then((res) => res.latestMasterVersion);
+
+const cluster = new gcp.container.Cluster('pokemon_api', {}, {});
